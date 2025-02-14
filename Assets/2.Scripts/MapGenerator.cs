5b32e7870dcb7d8728ab655973a7b75a982b81a4
김지훈 <ricon4246@gmail.com>
2025-02-14 11:48:00 +0900

    02. Octaves

--- Assets/2.Scripts/MapGenerator.cs
+++ Assets/2.Scripts/MapGenerator.cs
@@ -8,15 +8,43 @@ public class MapGenerator : MonoBehaviour
     [SerializeField] private int m_mapHeight;
     [SerializeField] private float m_noiseScale;
 
+    [SerializeField] private int m_octaves;
+    [Range(0, 1)]
+    [SerializeField] private float m_persistance;
+    [SerializeField] private float m_lacunarity;
+
+    [SerializeField] private int m_seed;
+    [SerializeField] private Vector2 m_offset;
+
     [Space, Header("Editor")]
     [SerializeField] private bool m_autoUpdate = true;
     public bool AutoUpdate => m_autoUpdate;
 
     public void GenerateMap()
     {
-        var noiseMap = Noise.GenerateNoiseMap(m_mapWidth, m_mapHeight, m_noiseScale);
+        var noiseMap = Noise.GenerateNoiseMap(m_mapWidth, m_mapHeight, m_seed, m_noiseScale, m_octaves, m_persistance, m_lacunarity, m_offset);
 
         var display = FindObjectOfType<MapDisplay>();
         display.DrawNoiseMap(noiseMap);
+    }
+
+    private void OnValidate()
+    {
+        if (m_mapWidth < 1)
+        {
+            m_mapWidth = 1;
+        }
+        if (m_mapHeight < 1)
+        {
+            m_mapHeight = 1;
+        }
+        if(m_lacunarity < 1)
+        {
+            m_lacunarity = 1;
+        }
+        if(m_octaves < 0)
+        {
+            m_octaves = 0;
+        }
     }
 }
