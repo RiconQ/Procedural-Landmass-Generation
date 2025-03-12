9d94b4cc1a0e30b1c7b8fe0b6e1e8b4389b821ad
jihun.kim <64973117+RiconQ@users.noreply.github.com>
2025-03-12 11:45:28 +0900

    [feat] Normals

---
+++ Assets/02.Scripts/MapGenerator.cs
@@ -0,0 +1,237 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using System;
+
+public class MapGenerator : MonoBehaviour
+{
+    public const int MAP_CHUNK_SIZE = 239;
+
+    private enum DrawMode
+    {
+        NoiseMap,
+        ColorMap,
+        Mesh,
+        FalloffMap
+    }
+
+    #region Inspector Variables
+    [SerializeField] private float m_noiseScale;
+
+    [SerializeField] private int m_octaves;
+    [Range(0, 1)]
+    [SerializeField] private float m_persistance;
+    [SerializeField] private float m_lacunarity;
+
+    [SerializeField] private int m_seed;
+    [SerializeField] private Vector2 m_offset;
+    [SerializeField] private Noise.ENormalizeMode m_normalizeMode;
+
+    [Space(20)]
+    [Tooltip("숫자가 작아질수록 디테일업")]
+    [Range(0, 6)]
+    [SerializeField] private int m_editorPreviewLOD;
+
+    [Space(20)]
+    [SerializeField] private TerrainType[] m_regions;
+
+    [Space(20)]
+    [SerializeField] private float m_meshHeightMultiplier;
+    [SerializeField] private AnimationCurve m_meshHeightCurve;
+
+    [Space(20)]
+    [SerializeField] private bool m_autoUpdate = true;
+    [SerializeField] private DrawMode m_drawMode;
+
+    [Space(20)]
+    [SerializeField] private bool m_useFalloff;
+    private float[,] m_falloffMap;
+
+    public bool AutoUpdate => m_autoUpdate;
+    #endregion
+
+
+    private Queue<MapThreadInfo<MapData>> m_mapDataThreadInfoQueue = new Queue<MapThreadInfo<MapData>>();
+    private Queue<MapThreadInfo<MeshData>> m_meshDataThreadInfoQueue = new Queue<MapThreadInfo<MeshData>>();
+
+    private void Awake()
+    {
+        m_falloffMap = FalloffGenerator.GenerateFalloffMap(MAP_CHUNK_SIZE);
+    }
+    private void Update()
+    {
+        if (m_mapDataThreadInfoQueue.Count > 0)
+        {
+            for (int i = 0; i < m_mapDataThreadInfoQueue.Count; i++)
+            {
+                var threadInfo = m_mapDataThreadInfoQueue.Dequeue();
+                threadInfo.m_callback(threadInfo.m_parameter);
+            }
+        }
+
+        if (m_meshDataThreadInfoQueue.Count > 0)
+        {
+            for (int i = 0; i < m_meshDataThreadInfoQueue.Count; i++)
+            {
+                var threadInfo = m_meshDataThreadInfoQueue.Dequeue();
+                threadInfo.m_callback(threadInfo.m_parameter);
+            }
+        }
+    }
+
+    public void DrawMapInEditor()
+    {
+        var mapData = GenerateMapData(Vector2.zero);
+
+        var display = FindObjectOfType<MapDisplay>();
+        if (m_drawMode == DrawMode.NoiseMap)
+        {
+            display.DrawTexture(TextureGenerator.TextureFromHeightMap(mapData.m_heightMap));
+        }
+        else if (m_drawMode == DrawMode.ColorMap)
+        {
+            display.DrawTexture(TextureGenerator.TextureFromColorMap(mapData.m_colorMap, MAP_CHUNK_SIZE, MAP_CHUNK_SIZE));
+        }
+        else if (m_drawMode == DrawMode.Mesh)
+        {
+            display.DrawMesh(MeshGenerator.GenerateTerrainMesh(mapData.m_heightMap, m_meshHeightMultiplier, m_meshHeightCurve, m_editorPreviewLOD),
+                TextureGenerator.TextureFromColorMap(mapData.m_colorMap, MAP_CHUNK_SIZE, MAP_CHUNK_SIZE));
+        }
+        else if (m_drawMode == DrawMode.FalloffMap)
+        {
+            display.DrawTexture(TextureGenerator.TextureFromHeightMap(FalloffGenerator.GenerateFalloffMap(MAP_CHUNK_SIZE)));
+        }
+    }
+
+
+    #region Thread 사용
+
+    public void RequestMapData(Vector2 center, Action<MapData> callback)
+    {
+        ThreadStart threadStart = () => MapDataThread(center, callback);
+
+        new Thread(threadStart).Start();
+    }
+    private void MapDataThread(Vector2 center, Action<MapData> callback)
+    {
+        var mapData = GenerateMapData(center);
+        lock (m_mapDataThreadInfoQueue)
+        {
+            m_mapDataThreadInfoQueue.Enqueue(new MapThreadInfo<MapData>(callback, mapData));
+        }
+    }
+
+    public void RequestMeshData(MapData mapData, int lod, Action<MeshData> callback)
+    {
+        ThreadStart threadStart = () => MeshDataThread(mapData, lod, callback);
+
+        new Thread(threadStart).Start();
+    }
+
+    private void MeshDataThread(MapData mapData, int lod, Action<MeshData> callback)
+    {
+        var meshData = MeshGenerator.GenerateTerrainMesh(mapData.m_heightMap, m_meshHeightMultiplier, m_meshHeightCurve, lod);
+        lock (m_meshDataThreadInfoQueue)
+        {
+            m_meshDataThreadInfoQueue.Enqueue(new MapThreadInfo<MeshData>(callback, meshData));
+        }
+    }
+    #endregion
+
+    #region Unitask 사용
+    /*
+    public async UniTask<MapData> RequestMapData()
+    {
+        return await UniTask.RunOnThreadPool(() => GenerateMapData());
+    }
+
+    public async UniTask<MeshData> RequestMeshData(MapData mapData)
+    {
+        return await UniTask.RunOnThreadPool(() =>
+        MeshGenerator.GenerateTerrainMesh(mapData.m_heightMap, m_meshHeightMultiplier, m_meshHeightCurve, m_levelOfDetail);
+        );
+    }*/
+
+    #endregion
+
+    private MapData GenerateMapData(Vector2 center)
+    {
+        var noiseMap = Noise.GenerateNoiseMap(
+            MAP_CHUNK_SIZE + 2, MAP_CHUNK_SIZE + 2, m_seed, m_noiseScale, m_octaves, m_persistance, m_lacunarity, center + m_offset, m_normalizeMode);
+
+        //Color Map 설정
+        var colorMap = new Color[MAP_CHUNK_SIZE * MAP_CHUNK_SIZE];
+        for (int y = 0; y < MAP_CHUNK_SIZE; y++)
+        {
+            for (int x = 0; x < MAP_CHUNK_SIZE; x++)
+            {
+                if (m_useFalloff)
+                {
+                    noiseMap[x, y] = Mathf.Clamp01(noiseMap[x, y] - m_falloffMap[x, y]);
+                }
+                var currentHeight = noiseMap[x, y];
+                for (int i = 0; i < m_regions.Length; i++)
+                {
+                    if (currentHeight >= m_regions[i].height)
+                    {
+                        colorMap[y * MAP_CHUNK_SIZE + x] = m_regions[i].color;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+
+        return new MapData(noiseMap, colorMap);
+    }
+
+    private void OnValidate()
+    {
+        if (m_lacunarity < 1)
+        {
+            m_lacunarity = 1;
+        }
+        if (m_octaves < 0)
+        {
+            m_octaves = 0;
+        }
+
+        m_falloffMap = FalloffGenerator.GenerateFalloffMap(MAP_CHUNK_SIZE);
+    }
+
+    private struct MapThreadInfo<T>
+    {
+        public readonly Action<T> m_callback;
+        public readonly T m_parameter;
+
+        public MapThreadInfo(Action<T> callback, T parameter)
+        {
+            m_callback = callback;
+            m_parameter = parameter;
+        }
+    }
+}
+
+[System.Serializable]
+public struct TerrainType
+{
+    public string name;
+    public float height;
+    public Color color;
+}
+
+public struct MapData
+{
+    public readonly float[,] m_heightMap;
+    public readonly Color[] m_colorMap;
+
+    public MapData(float[,] heightMap, Color[] colorMap)
+    {
+        this.m_heightMap = heightMap;
+        this.m_colorMap = colorMap;
+    }
+}
---
+++ Assets/02.Scripts/MeshGenerator.cs
@@ -0,0 +1,208 @@
+using UnityEngine;
+
+/// <summary>
+/// heightMap을 바탕으로 Vertex를 생성
+/// Vertex를 Triangle로 연결해서 Mesh를 만든다
+///     Unity에서 메시를 직접 생성하려면 -> Vertex를 배치하고 Triangle로 연결해야한다
+/// </summary>
+public static class MeshGenerator
+{
+    /// <summary>
+    /// heightMap을 바탕으로 3D 지형을 생성
+    /// </summary>
+    /// <param name="heightMap">해당 위치의 높이(Terrain의 높낮이 데이터)</param>
+    /// <returns>Mesh의 점, 삼각형, UV데이터를 담는 Class</returns>
+    public static MeshData GenerateTerrainMesh(float[,] heightMap, float heightMultiplier, AnimationCurve _heightCurve, int levelOfDetail)
+    {
+        var heightCurve = new AnimationCurve(_heightCurve.keys);
+        var meshSimplificationIncrement = (levelOfDetail == 0) ? 1 : levelOfDetail * 2;
+
+        var borderedSize = heightMap.GetLength(0);
+        var meshSize = borderedSize - 2 * meshSimplificationIncrement;
+        var meshSizeUnsimplified = borderedSize - 2;
+
+        var topLeftX = (meshSizeUnsimplified - 1) / -2f;
+        var topLeftZ = (meshSizeUnsimplified - 1) / 2f;
+
+        var verticePerLine = ((meshSize - 1) / meshSimplificationIncrement) + 1;
+
+        var meshData = new MeshData(verticePerLine);
+
+        var vertexIndicesMap = new int[borderedSize, borderedSize];
+        var meshVertexIndex = 0;
+        var borderVertexIndex = -1;
+
+        for (int y = 0; y < borderedSize; y += meshSimplificationIncrement)
+        {
+            for (int x = 0; x < borderedSize; x += meshSimplificationIncrement)
+            {
+                var isBorderVertex = y == 0 || y == borderedSize - 1 || x == 0 || x == borderedSize - 1;
+
+                if (isBorderVertex)
+                {
+                    vertexIndicesMap[x, y] = borderVertexIndex;
+                    borderVertexIndex--;
+                }
+                else
+                {
+                    vertexIndicesMap[x, y] = meshVertexIndex;
+                    meshVertexIndex++;
+                }
+            }
+        }
+
+
+        for (int y = 0; y < borderedSize; y += meshSimplificationIncrement)
+        {
+            for (int x = 0; x < borderedSize; x += meshSimplificationIncrement)
+            {
+                var vertexIndex = vertexIndicesMap[x, y];
+                var percent = new Vector2((x - meshSimplificationIncrement) / (float)meshSize, (y - meshSimplificationIncrement) / (float)meshSize);
+                var height = heightCurve.Evaluate(heightMap[x, y]) * heightMultiplier;
+                var vertexPosition = new Vector3(topLeftX + percent.x * meshSizeUnsimplified, height, topLeftZ - percent.y * meshSizeUnsimplified);
+
+                meshData.AddVertex(vertexPosition, percent, vertexIndex);
+
+                if (x < borderedSize - 1 && y < borderedSize - 1)
+                {
+                    var a = vertexIndicesMap[x, y];
+                    var b = vertexIndicesMap[x + meshSimplificationIncrement, y];
+                    var c = vertexIndicesMap[x, y + meshSimplificationIncrement];
+                    var d = vertexIndicesMap[x + meshSimplificationIncrement, y + meshSimplificationIncrement];
+                    meshData.AddTriangle(a, d, c);
+                    meshData.AddTriangle(d, a, b);
+                }
+
+                vertexIndex++;
+            }
+        }
+        return meshData;
+    }
+}
+
+
+public class MeshData
+{
+    private Vector3[] m_vertices;
+    private int[] m_triangles;
+    private Vector2[] m_uvs;
+
+    private Vector3[] m_borderVertices;
+    private int[] m_borderTriangles;
+
+    private int m_triangleIndex;
+    private int m_borderTriangleIndex;
+
+    public MeshData(int verticesPerLine)
+    {
+        m_vertices = new Vector3[verticesPerLine * verticesPerLine];
+        m_uvs = new Vector2[verticesPerLine * verticesPerLine];
+        m_triangles = new int[(verticesPerLine - 1) * (verticesPerLine - 1) * 6];
+
+        m_borderVertices = new Vector3[verticesPerLine * 4 + 4];
+        m_borderTriangles = new int[24 * verticesPerLine];
+    }
+
+    public void AddVertex(Vector3 vertexPosition, Vector2 uv, int vertexIndex)
+    {
+        if (vertexIndex < 0)
+        {
+            m_borderVertices[-vertexIndex - 1] = vertexPosition;
+        }
+        else
+        {
+            m_vertices[vertexIndex] = vertexPosition;
+            m_uvs[vertexIndex] = uv;
+        }
+    }
+
+    //MeshData에 삼각형 추가
+    public void AddTriangle(int a, int b, int c)
+    {
+        if (a < 0 || b < 0 || c < 0)
+        {
+            m_borderTriangles[m_borderTriangleIndex] = a;
+            m_borderTriangles[m_borderTriangleIndex + 1] = b;
+            m_borderTriangles[m_borderTriangleIndex + 2] = c;
+
+            m_borderTriangleIndex += 3;
+        }
+        else
+        {
+            m_triangles[m_triangleIndex] = a;
+            m_triangles[m_triangleIndex + 1] = b;
+            m_triangles[m_triangleIndex + 2] = c;
+
+            m_triangleIndex += 3;
+        }
+    }
+
+    private Vector3[] CalculateNormals()
+    {
+        var vertexNormals = new Vector3[m_vertices.Length];
+        var triangleCount = m_triangles.Length / 3;
+        for (int i = 0; i < triangleCount; i++)
+        {
+            var normalTriangleIndex = i * 3;
+            var vertexIndexA = m_triangles[normalTriangleIndex];
+            var vertexIndexB = m_triangles[normalTriangleIndex + 1];
+            var vertexIndexC = m_triangles[normalTriangleIndex + 2];
+
+            var triangleNormal = SurfaceNormalFromIndices(vertexIndexA, vertexIndexB, vertexIndexC);
+            vertexNormals[vertexIndexA] += triangleNormal;
+            vertexNormals[vertexIndexB] += triangleNormal;
+            vertexNormals[vertexIndexC] += triangleNormal;
+        }
+
+        var borderTriangleCount = m_borderTriangles.Length / 3;
+        for (int i = 0; i < borderTriangleCount; i++)
+        {
+            var normalTriangleIndex = i * 3;
+            var vertexIndexA = m_borderTriangles[normalTriangleIndex];
+            var vertexIndexB = m_borderTriangles[normalTriangleIndex + 1];
+            var vertexIndexC = m_borderTriangles[normalTriangleIndex + 2];
+
+            var triangleNormal = SurfaceNormalFromIndices(vertexIndexA, vertexIndexB, vertexIndexC);
+            if (vertexIndexA >= 0)
+            {
+                vertexNormals[vertexIndexA] += triangleNormal;
+            }
+            if (vertexIndexB >= 0)
+            {
+                vertexNormals[vertexIndexB] += triangleNormal;
+            }
+            if (vertexIndexC >= 0)
+            {
+                vertexNormals[vertexIndexC] += triangleNormal;
+            }
+        }
+
+        for (int i = 0; i < vertexNormals.Length; i++)
+        {
+            vertexNormals[i].Normalize();
+        }
+        return vertexNormals;
+    }
+
+    private Vector3 SurfaceNormalFromIndices(int indexA, int indexB, int indexC)
+    {
+        var pointA = (indexA < 0) ? m_borderVertices[-indexA - 1] : m_vertices[indexA];
+        var pointB = (indexB < 0) ? m_borderVertices[-indexB - 1] : m_vertices[indexB];
+        var pointC = (indexC < 0) ? m_borderVertices[-indexC - 1] : m_vertices[indexC];
+
+        var sideAB = pointB - pointA;
+        var sideAC = pointC - pointA;
+
+        return Vector3.Cross(sideAB, sideAC);
+    }
+
+    public Mesh CreateMesh()
+    {
+        var mesh = new Mesh();
+        mesh.vertices = m_vertices;
+        mesh.triangles = m_triangles;
+        mesh.uv = m_uvs;
+        mesh.normals = CalculateNormals();
+        return mesh;
+    }
+}
